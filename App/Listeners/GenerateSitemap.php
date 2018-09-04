96ab431bf4ad68510fafeca797e18f9fcf777315
Jason Beggs <jasonlbeggs@gmail.com>
2018-09-04 08:09:10 -0400

    Fix formatting in GenerateSitemap.php

--- App/Listeners/GenerateSitemap.php
+++ App/Listeners/GenerateSitemap.php
@@ -1,16 +1,16 @@
 <?php
- namespace App\Listeners;
+namespace App\Listeners;
 
 use TightenCo\Jigsaw\Jigsaw;
- use samdark\sitemap\Sitemap;
+use samdark\sitemap\Sitemap;
 
- class GenerateSitemap
- {
-     public function handle(Jigsaw $jigsaw)
-     {
-         $baseUrl = $jigsaw->getConfig('baseUrl');
-         $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
-         collect($jigsaw->getOutputPaths())
+class GenerateSitemap
+{
+    public function handle(Jigsaw $jigsaw)
+    {
+        $baseUrl = $jigsaw->getConfig('baseUrl');
+        $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
+        collect($jigsaw->getOutputPaths())
             ->map(function ($path) {
                 if ($path) {
                     return str_start($path, '/');
@@ -21,12 +21,13 @@ use TightenCo\Jigsaw\Jigsaw;
                     $sitemap->addItem($baseUrl . $path, time(), Sitemap::DAILY);
                 }
             });
-         $sitemap->write();
-     }
-     public function isAsset($path)
-     {
-         return collect(['/css', '/img', '/js', '/CNAME'])->contains(function ($value) use ($path) {
-             return starts_with($path, $value);
-         });
-     }
- }
+        $sitemap->write();
+    }
+
+    public function isAsset($path)
+    {
+        return collect(['/css', '/img', '/js', '/CNAME'])->contains(function ($value) use ($path) {
+            return starts_with($path, $value);
+        });
+    }
+}
