935ca5fa8efe316732ec05c1b5b0fc28e13007e1
Jason Beggs <jasonlbeggs@gmail.com>
2018-09-04 08:10:36 -0400

    Fix formatting in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,4 @@ use TightenCo\Jigsaw\Jigsaw;
 
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
- $events->afterBuild(App\Listeners\GenerateSitemap::class);
+$events->afterBuild(App\Listeners\GenerateSitemap::class);
