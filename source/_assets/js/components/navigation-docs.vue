8004d5e7004e0bb782e388eccf783226ada96d19
Anthony Terrell <ant@Houston.local>
2018-09-19 13:10:52 -0400

    Update typography styles

--- source/_assets/js/components/navigation-docs.vue
+++ source/_assets/js/components/navigation-docs.vue
@@ -2,7 +2,7 @@
     <div class="w-1/3 pl-6 hidden md:flex">
         <nav class="flex flex-col" v-if="headings.length">
             <p class="text-sm uppercase text-grey-dark mb-6">On this page</p>
-            <a :href="'#' + heading.id" :key="heading.id" class="text-blue-darker text-sm mb-4"
+            <a :href="'#' + heading.id" :key="heading.id" class="text-blue text-sm mb-4"
                 v-for="heading in headings">{{ heading.textContent }}</a>
         </nav>
     </div>
