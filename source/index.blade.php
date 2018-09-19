e671d36da578027dc011cec054bd55d2a57cbc9b
Anthony Terrell <anthonygterrell@gmail.com>
2018-09-19 14:45:51 -0400

    Move landing navigation to index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,28 @@
 
 @section('body')
 <div class="pt-6 font-normal text-xl bg-brown-lightest">
-    @include('_components.navigation')
+    <header class="container-content">
+        <nav class="flex justify-between items-center mb-8" aria-role="navigation">
+            <div class="flex items-center">
+                <img src="{{ $page->asset_prefix }}/assets/img/jigsaw-logo.svg" alt="Jigsaw logo"
+                    class="logo-icon rounded-lg shadow border-2 border-white mr-2 sm:mr-4 w-10 sm:w-12" />
+
+                <h4 class="title-4 uppercase tracking-wide text-blue-darker">Jigsaw</h4>
+            </div>
+
+            <div class="text-sm tracking-wide">
+                <a href="https://github.com/tightenco/jigsaw" title="Contribute to Jigsaw on GitHub"
+                    class="mr-4 text-blue-darker uppercase font-light">
+                    <span class="hidden md:inline-block">Contribute</span>
+
+                    <img src="/assets/img/GitHub.svg" alt="GitHub alien logo" class="md:hidden -mb-1">
+                </a>
+
+                <a href="/docs/installation" title="Read the Jigsaw documentation"
+                    class="py-2 px-4 md:py-3 md:px-4 bg-purple rounded text-white uppercase font-light shadow-md">Docs</a>
+            </div>
+        </nav>
+    </header>
 
     <div class="container-content pt-4 sm:pb-12">
         <div class="flex-col mb-8 pb-4">
@@ -18,13 +39,9 @@
     </div>
 
     @include('_components.jigsaw-is-awesome')
-
     @include('_components.getting-started')
-
     @include('_components.features')
-
     @include('_components.compile-your-assets')
-
     @include('_components.build-your-site')
 </div>
 
