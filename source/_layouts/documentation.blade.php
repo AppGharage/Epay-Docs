434f0080aa1665af7684e58c3027a60fae8d135a
Anthony Terell <anthonygterrell@gmail.com>
2018-09-19 20:46:45 -0400

    Remove documenation nav component.

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -1,7 +1,29 @@
 @extends('_layouts.master')
 
 @section('body')
-@include('_components.documentation.navigation')
+<nav class="flex items-center py-2 px-3 container-content" aria-role="navigation">
+    <a href="/" class="flex items-center lg:w-1/3" title="Jigsaw by Tighten">
+        <img src="{{ $page->asset_prefix }}/assets/img/jigsaw-logo.svg" alt="Jigsaw logo"
+            class="rounded-lg w-8 md:w-10 mr-2 md:mr-4" />
+
+        <h4 class="hidden lg:inline-block mr-4 title-4 uppercase tracking-wide text-blue-darker">Jigsaw</h4>
+    </a>
+
+    <input id="docsearch" type="text" class="" />
+
+    <div class="w-1/3 hidden lg:flex items-center pl-8">
+        <a href="https://github.com/tightenco/jigsaw" title="Contribute to Jigsaw on GitHub"
+            class="mr-4 text-blue-darker">
+            <img src="/assets/img/GitHub.svg" alt="GitHub alien logo">
+        </a>
+
+        <p class="text-sm text-blue-docs-dark">
+            A project by
+            <a href="https://tighten.co" title="Tighten | Product Development for Web + Mobile"
+                class="text-purple">Tighten</a>
+        </p>
+    </div>
+</nav>
 
 <div class="bg-brown-lightest min-h-screen pt-4 md:pt-8">
     <div class="flex py-2 px-3 container-content lg:px-0">
