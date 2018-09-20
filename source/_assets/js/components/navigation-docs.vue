647cfca09bae8e41e5c4b45f97801b294b8cb75e
Anthony Terrell <anthonygterrell@gmail.com>
2018-09-20 10:51:16 -0400

    Less file cleanup

--- source/_assets/js/components/navigation-docs.vue
+++ source/_assets/js/components/navigation-docs.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-1/3 pl-6 hidden lg:flex">
-        <nav class="flex flex-col" v-if="headings.length">
+        <nav role="aside" class="flex flex-col" v-if="headings.length">
             <p class="text-sm uppercase text-grey-dark mb-6">On this page</p>
 
             <a :href="'#' + heading.id" :key="heading.id" class="text-blue text-sm mb-4"
