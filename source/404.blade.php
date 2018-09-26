fece5279e1f8be85a898b05e3e7e0a4f2f61392e
Anthony Terrell <anthonygterrell@gmail.com>
2018-09-26 11:48:20 -0400

    Update to 404 blade

--- source/404.blade.php
+++ source/404.blade.php
@@ -8,12 +8,12 @@ permalink: 404.html
 <div class="min-h-screen pt-6 font-normal text-xl bg-brown-lightest">
     @include('_components.navigation')
 
-    <div class="flex mt-32 max-w-xl mx-auto justify-around">
+    <div class="flex py-32 max-w-xl mx-auto justify-around">
         <img src="/assets/img/404.svg" alt="404 Jigsaw">
 
         <div>
             <h1 class="text-6xl font-bold text-blue-darker leading-0 mb-12">404</h1>
-            <h1 class="text-5xl font-bold text-blue-darker mb-4">Page not found</h1>
+            <h1 class="text-5xl font-bold text-blue-darker mb-6">Page not found</h1>
             <p class="text-grey-dark text-lg mb-6">The page you requested could not be found</p>
 
             <a href="/" title="Jigsaw by Tighten" class="bg-purple-dark py-2 px-6 text-sm rounded-lg uppercase text-white shadow">Go to home</a>
