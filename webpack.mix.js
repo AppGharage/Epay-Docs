348719c1ba4a401ab34173572882fb09a74fdec2
Keith Damiani <keith@tighten.co>
2018-12-05 15:33:12 -0500

    Add PurgeCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,21 +7,21 @@ mix.disableSuccessNotifications();
 mix.setPublicPath("source/assets/build");
 
 mix.webpackConfig({
-  plugins: [
-    build.jigsaw,
-    build.browserSync(),
-    build.watch(["source/**/*.md", "source/**/*.php", "source/**/*.less"])
-  ]
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch(["source/**/*.md", "source/**/*.php", "source/**/*.less"])
+    ]
 });
 
 mix.js('source/_assets/js/app.js', 'js/')
-  .less("source/_assets/less/main.less", "css/")
-  .options({
-    proceslessUrls: false,
-    postCss: [tailwindcss("tailwind.js")]
-  })
-  .version();
+    .less("source/_assets/less/main.less", "css/")
+    .options({
+        postCss: [tailwindcss("tailwind.js")]
+    })
+    .purgeCss()
+    .version();
 
-if (!mix.inProduction()) {
-  mix.sourceMaps();
+if (! mix.inProduction()) {
+    mix.sourceMaps();
 }
