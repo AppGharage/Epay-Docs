c6180ab43045aeaa9de8f9b5d00706c6b03746e0
Keith Damiani <keith@tighten.co>
2018-12-20 13:12:20 -0500

    Fix spacing/sizing/centering of logos in "compile" section on index page

--- source/_components/compile-your-assets.blade.php
+++ source/_components/compile-your-assets.blade.php
@@ -2,21 +2,20 @@
     <h3 class="text-blue-darker text-center text-3xl mb-6">Compile your assets using Laravel Mix.</h3>
 
     <p class="max-w-md mx-auto text-center text-blue text-lg leading-normal mb-12">
-        Jigsaw bakes in support for Laravel Mix so you can compile your CSS <br class="hidden md:block"/>
-        and Javascript assets the same way you're used to in Laravel.
+        Jigsaw bakes in support for Laravel Mix so you can compile your CSS and Javascript assets the same way you're used to in Laravel.
     </p>
 
-    <div class="flex flex-wrap max-w-xs sm:max-w-xl px-8">
-        <img class="w-1/2 sm:w-1/4 p-3 sm:p-4 md:p-6 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter rounded-l-lg" src="/assets/img/less-logo.svg" alt="Less logo">
+    <div class="flex flex-wrap max-w-xs sm:max-w-xl px-0 sm:px-8">
+        <img class="w-1/2 sm:w-1/4 py-5 px-4 sm:px-8 md:px-12 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter rounded-l-lg" src="/assets/img/less-logo.svg" alt="Less logo">
 
-        <div class="w-1/2 sm:w-1/4 flex flex-col items-center p-3 sm:p-4 md:p-6 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter">
+        <div class="w-1/2 sm:w-1/4 flex flex-col items-center justify-center py-5 px-0 sm:px-4 md:px-6 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter">
             <img class="w-1/3 mt-2" src="/assets/img/tailwind-logo.svg" alt="Tailwind logo">
 
-            <p class="text-lg lg:text-2xl text-blue-darker font-semibold pt-2 mb-0">Tailwind CSS</p>
+            <p class="text-xl sm:text-base md:text-xl lg:text-2xl text-blue-darker font-semibold leading-tight pt-2 mb-0">Tailwind CSS</p>
         </div>
 
-        <img class="w-1/2 sm:w-1/4 p-3 sm:p-4 md:p-6 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter" src="/assets/img/webpack-logo.svg" alt="Webpack logo">
+        <img class="w-1/2 sm:w-1/4 py-5 px-0 sm:px-4 md:px-6 sm:bg-white sm:border sm:border-r-0 border-indigo-lighter" src="/assets/img/webpack-logo.svg" alt="Webpack logo">
 
-        <img class="w-1/2 sm:w-1/4 p-3 sm:p-4 md:p-6 sm:bg-white sm:border border-indigo-lighter rounded-r-lg" src="/assets/img/sass-logo.svg" alt="Sass logo">
+        <img class="w-1/2 sm:w-1/4 py-5 px-6 sm:px-8 md:px-12 sm:bg-white sm:border border-indigo-lighter rounded-r-lg" src="/assets/img/sass-logo.svg" alt="Sass logo">
     </div>
 </section>
